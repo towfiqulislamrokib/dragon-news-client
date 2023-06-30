1e981943a3ebbdbbd06a5b141af9553da7a2fe9f
Towfiqul <rokib.ewubd.15@gmail.com>
2023-06-30 18:41:17 +0600

    after depoloy

--- src/Pages/Shared/LeftsideNav/LeftsideNav.js
+++ src/Pages/Shared/LeftsideNav/LeftsideNav.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Leftsidenav = () => {
     const [categories, setCategories] = useState([]);
     useEffect( () => {
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://dragon-news-server-psi-henna.vercel.app/news-categories')
         .then(res => res.json())
         .then( data => setCategories(data));
     } ,[])
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -16,17 +16,17 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-psi-henna.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-psi-henna.vercel.app/category/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoutes><News></News></PrivateRoutes>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-psi-henna.vercel.app/news/${params.id}`)
             },
             {
                 path: '/login',
